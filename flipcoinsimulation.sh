d34f9695cbe959bb5428d96afc4f27560a5ea52c
unknown <you@example.com>
2021-02-19 16:32:24 +0530

    Added: code of heads or tail

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,1 +1,10 @@
-echo "Problems on flip coin simulation"
+#!/bin/bash
+head=1
+tail=0
+randomNumber=$(($RANDOM%2))
+if [ $randomNumber -eq 1 ]
+then 
+  echo "The winner is Heads"
+else
+ echo "The winner is Tails"
+fi
