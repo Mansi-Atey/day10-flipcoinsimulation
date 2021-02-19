5baa4b3c6a0aaaf2d96374bfa2fed4bf209ac7a9
unknown <you@example.com>
2021-02-19 16:43:52 +0530

    Added: no of times heads or tail won

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -8,3 +8,18 @@ then
 else
  echo "The winner is Tails"
 fi
+totalHead=0
+totalTail=0
+
+for ((i=1;i<=15;i++))
+do
+    randomNumber=$(($RANDOM%2))
+if [ $randomNumber -eq 1 ]
+then
+totalHead=$(($totalHead+1))
+else
+totalTail=$(($totalTail+1))
+fi
+done
+echo "No. of times Head won =" $totalHead
+echo "No. of times Tail won =" $totalTail
